25c4d84461d90baf66adf3089e1bd4abd1cba2c5
CV Chiranthan <42867826+chiranthancv95@users.noreply.github.com>
2021-07-24 03:35:33 +0530

    Update data_transforms.py

--- tiny_imagenet_wrapper/data/data_transforms.py
+++ tiny_imagenet_wrapper/data/data_transforms.py
@@ -46,7 +46,7 @@ def albumentations_transforms(p=1.0, is_train=False):
         A.RandomCrop (32, 32, always_apply=True, p=1.0),
         A.HorizontalFlip(p=0.5),
         A.CoarseDropout(max_holes = 1, max_height=16, max_width=16, min_holes = 1, min_height=16, min_width=16, fill_value=(0.4914, 0.4822, 0.4465), mask_fill_value = None),
-        ToTensor()
+        ToTensorV2()
     ]
     transforms_result = A.Compose(train_transforms)
     return lambda img:transforms_result(image=np.array(img))["image"]
@@ -57,7 +57,7 @@ def albumentations_transforms_test():
 
     test_transform = [
         A.Normalize(mean=mean, std=std),
-        ToTensor()]
+       ToTensorV2()]
     transforms_result = A.Compose(test_transform)
     return lambda img:transforms_result(image=np.array(img))["image"]
 
